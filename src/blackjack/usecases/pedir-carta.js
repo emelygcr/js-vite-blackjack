0215f7ec029dca8c892605c59fa41a237554c9fd
Emely Cruz <emelygcr@gmail.com>
2024-10-07 14:35:46 -0600

    First commit

---
+++ src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,16 @@
+
+/**
+ * 
+ * @param {Array<string>} deck  es un arreglo de string
+ * @returns {String} retorna la carta del deck
+ */
+export const pedirCarta = (deck) => {
+
+
+    if ( !deck || deck.length === 0 ) {
+        throw new Error ('No hay cartas en el deck');
+    }
+    const carta = deck.pop();
+    return carta;
+}
+
